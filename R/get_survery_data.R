168bada687374a5f36fb67173f4e4f548730b60a
Maximilian Hesselbarth <mhk.hesselbarth@gmail.com>
2020-10-11 17:02:11 -0400

    Add further results of survery

---
+++ R/get_survery_data.R
@@ -0,0 +1,17 @@
+# Make sure survery data is up-to date
+
+library(googlesheets4)
+
+answers <- read_sheet("https://docs.google.com/spreadsheets/d/1mV_wZWb4desjdzUWHALIB-uFRCauSTOGSvENsnkiAhI/edit#gid=1189808333")
+
+old_names <- names(answers)
+
+new_names <- c("time", "position", "usage_freq", "expertise", "major_topic",
+               "important_tasks", "data_model", "r_packages", "own_package",
+               "own_package_location", "missing_methods", "usefulness", "comments")
+
+names(answers) <-  new_names
+
+results <- list(answers = answers, old_names = old_names, new_names = new_names)
+
+saveRDS(object = results, file = "paper/results.rds")
